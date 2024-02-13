a8eb7598db9fecf043a5f50d26455f6f2a21b8dc
Kunisettisudhamanikanta <sudhamanikanta@gmail.com>
2024-02-13 21:43:30 +0530

    Added the url and code for API

--- AutomationTest/src/main/java/com/bdd/actionPages/APITest.java
+++ AutomationTest/src/main/java/com/bdd/actionPages/APITest.java
@@ -2,6 +2,7 @@ package com.bdd.actionPages;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
@@ -14,11 +15,24 @@ import static io.restassured.RestAssured.given;
 public class APITest extends BaseClass {
     public static WebDriver driver;
 
-    @Test
-    public  void Test_API(){
+    public static final String API_URL = "https://api.citybik.es/v2/networks";
 
 
-    }
+	@Test
+	public void Test_API() throws Exception {
+
+		Response response = RestAssured.get(API_URL);
+        System.out.println("Response Body:");
+        System.out.println(response.getBody().asString());
+
+        String countryName = "Germany";
+        String countryLatitude = response.path("networks.find { it.location.country == '" + countryName + "' }.location.latitude");
+        String countryLongitude = response.path("networks.find { it.location.country == '" + countryName + "' }.location.longitude");
+
+        System.out.println("Country: " + countryName);
+        System.out.println("Latitude: " + countryLatitude);
+        System.out.println("Longitude: " + countryLongitude);
+	}
 
 }
 
